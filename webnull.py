22207b3c3d167c81102053b053202330824e0656
MacRae Linton <macrae.linton@gmail.com>
2016-12-15 00:43:18 -0800

    Now it can re-enable access to a site with -e

--- webnull.py
+++ webnull.py
@@ -5,11 +5,13 @@ import urlparse
 import re
 
 SHIBBOLETH = "## webnull will only write below this line ##"
-HOSTFILE_PATH = "/etc/hosts"
+# HOSTFILE_PATH = "/etc/hosts"
+HOSTFILE_PATH = "./fakehosts"
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='A tool for putting websites into a black hole.')
     parser.add_argument('sitename', help='The website to be blackholed. Will be stripped down to just the hostname')
+    parser.add_argument('-e', '--enable', action='store_true', help='re-enables access to sitename')
     return parser
 
 def parse_hostname(sitename):
@@ -48,8 +50,33 @@ def nullify_site(sitename):
             for www in ["", "www."]:
                 hostfile.write(null + www + hostname + "\n")
 
+def unblock_site(sitename):
+    print "UNBLOCKING"
+    hostname = parse_hostname(sitename)
+
+    hosts = ""
+    with open(HOSTFILE_PATH, "r+") as hostfile:
+        hosts = hostfile.read()
+
+    parts = hosts.split(SHIBBOLETH)
+    if len(parts) != 2:
+        print "Your hostsfile is not managed by webnull, we won't change anything"
+        exit(1)
+    else:
+        preroll = parts[0]
+        managed = parts[1]
+
+        null_matcher = r'\n([^#\n].*' + re.escape(hostname) + r')'
+        new_managed = re.sub(null_matcher, r'\n# \1', managed)
+
+        with open(HOSTFILE_PATH, "w") as hostfile:
+            hostfile.write(preroll + SHIBBOLETH + new_managed)
+
 
 if __name__ == "__main__":
     args = arg_parser().parse_args()
 
-    nullify_site(args.sitename)
+    if args.enable:
+        unblock_site(args.sitename)
+    else:
+        nullify_site(args.sitename)
