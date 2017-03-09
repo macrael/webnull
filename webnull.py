f9ce55d5f517e8b6f896da78b0d5aba47182c7f2
MacRae Linton <macrae.linton@gmail.com>
2017-03-09 02:13:33 -0800

    go hard on '

--- webnull.py
+++ webnull.py
@@ -13,18 +13,18 @@ HOST_MATCHER = r'^([^#\n].*{0})'
 COMMENTED_MATCHER = r'^#\s(.*{0})'
 
 class ManagedHostfile:
-    SHIBBOLETH = "\n## webnull will only write below this line ##\n"
-    HOSTFILE_PATH = "/etc/hosts" if "DEV_MODE" not in os.environ else "dummyhosts"
+    SHIBBOLETH = '\n## webnull will only write below this line ##\n'
+    HOSTFILE_PATH = '/etc/hosts' if 'DEV_MODE' not in os.environ else 'dummyhosts'
 
     def _head_and_tail(self):
-        with open(self.HOSTFILE_PATH, "r") as hostfile:
+        with open(self.HOSTFILE_PATH, 'r') as hostfile:
             hosts = hostfile.read()
             parts = hosts.split(self.SHIBBOLETH)
             assert(len(parts) < 3)
             assert(len(parts) > 0)
 
             preroll = parts[0]
-            managed = ""
+            managed = ''
             if len(parts) == 2:
                 managed = parts[1]
 
@@ -36,17 +36,17 @@ class ManagedHostfile:
 
     def write_body(self, new_body):
         head, _ = self._head_and_tail()
-        with open(self.HOSTFILE_PATH, "w") as hostfile:
+        with open(self.HOSTFILE_PATH, 'w') as hostfile:
             hostfile.write(head + self.SHIBBOLETH + new_body)
 
 def parse_hostname(sitename):
     parsed_url = urlparse.urlparse(sitename)
     hostname = parsed_url.netloc
     if hostname == '':
-        # If you pass in "facebook.com/foo" urlparse treats it all as the path
-        match = re.match("[^\/]+", parsed_url.path)
+        # If you pass in 'facebook.com/foo' urlparse treats it all as the path
+        match = re.match(r'[^/]+', parsed_url.path)
         if match == None:
-            print "ERROR: Unable to make the provided sitename into a hostname: " + parsed_url.path
+            print 'ERROR: Unable to make the provided sitename into a hostname: ' + parsed_url.path
             sys.exit(1)
         hostname = match.group(0)
 
@@ -66,19 +66,19 @@ def nullify_site(sitename):
     neuter_matcher = COMMENTED_MATCHER.format(hostname)
     if re.search(null_matcher, managed, flags=re.MULTILINE) != None:
         # if it's not commented, we ignore it
-        print hostname + " has already been sent to webnull"
+        print hostname + ' has already been sent to webnull'
         sys.exit(0)
     elif re.search(neuter_matcher, managed, flags=re.MULTILINE) != None:
         # if it's commented, we replace it
         managed = re.sub(neuter_matcher, r'\1', managed, flags=re.MULTILINE)
     else:
         # if it's not there, we write it.
-        ip5null = "127.0.0.1\t"
-        ip6null = "::1\t\t"
+        ip5null = '127.0.0.1\t'
+        ip6null = '::1\t\t'
 
         for null in [ip5null, ip6null]:
-            for www in ["", "www."]:
-                managed += (null + www + hostname + "\n")
+            for www in ['', 'www.']:
+                managed += (null + www + hostname + '\n')
 
     hostfile.write_body(managed)
 
@@ -88,8 +88,8 @@ def unblock_site(sitename):
     hostfile = ManagedHostfile()
     managed = hostfile.current_body()
 
-    if managed == "":
-        print "Your hostsfile is not managed by webnull, we won't change anything"
+    if managed == '':
+        print 'Your hostsfile is not managed by webnull, we won\'t change anything'
         sys.exit(1)
     else:
         null_matcher = HOST_MATCHER.format(hostname)
@@ -101,8 +101,8 @@ def unblock_all():
     hostfile = ManagedHostfile()
     managed = hostfile.current_body()
 
-    if managed == "":
-        print "Your hostsfile is not managed by webnull, we won't change anything"
+    if managed == '':
+        print 'Your hostsfile is not managed by webnull, we won\'t change anything'
         sys.exit(1)
     else: 
         new_managed = re.sub(r'^(.+)', r'# \1', managed, flags=re.MULTILINE)
@@ -113,8 +113,8 @@ def reblock_all():
     hostfile = ManagedHostfile()
     managed = hostfile.current_body()
 
-    if managed == "":
-        print "Your hostsfile is not managed by webnull, we won't change anything"
+    if managed == '':
+        print 'Your hostsfile is not managed by webnull, we won\'t change anything'
         sys.exit(1)
     else: 
         new_managed = re.sub(r'^#\s(.+)', r'\1', managed, flags=re.MULTILINE)
@@ -135,7 +135,7 @@ def reblock_timer(sitename, duration, all=False):
         sys.exit(0)
     signal.signal(signal.SIGINT, sigint_handler)
 
-    print sitename + " is enabled until " + str(datetime.datetime.now() + datetime.timedelta(minutes=duration))
+    print sitename + ' is enabled until ' + str(datetime.datetime.now() + datetime.timedelta(minutes=duration))
     time.sleep(duration * 60)
     if all:
         reblock_all()
@@ -143,7 +143,7 @@ def reblock_timer(sitename, duration, all=False):
         nullify_site(sitename)
 
 def enable_all(duration):
-    reblock_timer("ALL_ARE_ENABLED", duration, all=True)
+    reblock_timer('ALL_ARE_ENABLED', duration, all=True)
 
 
 def deny_site(args):
@@ -172,9 +172,9 @@ def arg_parser():
 
     return parser
 
-if __name__ == "__main__":
-    if "DEV_MODE" in os.environ:
-        print "Running in Development Mode"
+if __name__ == '__main__':
+    if 'DEV_MODE' in os.environ:
+        print 'Running in Development Mode'
 
     args = arg_parser().parse_args()
     args.func(args)
